31ae744389db8f34205906e405ca38f3894aac8d
Justin <snakejsj9@gmail.com>
2025-01-30 20:30:05 -0800

    port fix

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const client = new MongoClient(process.env.MONGO_URI);
 
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
 
 async function connectDB() {
